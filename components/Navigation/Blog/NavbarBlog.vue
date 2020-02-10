a01861fa1470c41c7db548c8edd0cdb709ed1c84
allan_oliveira <allanadriiano@gmail.com>
2020-02-10 12:35:45 -0300

    create navbar and menu mobile

---
+++ components/Navigation/Blog/NavbarBlog.vue
@@ -0,0 +1,30 @@
+<template>
+  <nav class="navbar-blog navbar">
+    <div class="container justify-content-flex-end">
+      <ul class="menu">
+        <hall-menu-items-blog />
+      </ul>
+    </div>
+  </nav>
+</template>
+
+<script>
+import HallMenuItemsBlog from './MenuItemsBlog'
+
+export default {
+  name: 'NavbarBlog',
+  components: {
+    HallMenuItemsBlog
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+@import '~/assets/scss/_variables.scss';
+
+.navbar-blog {
+  @media only screen and (max-width: $breakpoint-sm) {
+    display: none;
+  }
+}
+</style>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,6 +1,26 @@
 <template>
   <div>
-    Layout Default
+    <hall-navbar />
+    <br />
+    <br />
+    <br />
+    <br />
+    <br />
+    <br />
+    <hall-navbar-blog />
     <nuxt />
   </div>
 </template>
+
+<script>
+import HallNavbar from '@/components/Navigation/Navbar'
+import HallNavbarBlog from '@/components/Navigation/Blog/NavbarBlog'
+
+export default {
+  name: 'BlogLayout',
+  components: {
+    HallNavbar,
+    HallNavbarBlog
+  }
+}
+</script>
