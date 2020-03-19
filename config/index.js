2578edbc3d9e7329f8c382302e008f0d39d03376
allan_oliveira <allanadriiano@gmail.com>
2020-03-19 13:01:06 -0300

    config url callback

--- config/index.js
+++ config/index.js
@@ -1,5 +1,9 @@
 export default {
   app: {
-    url: `${process.env.VUE_APP_BASE_URL || 'https://hallofjustice.com.br'}`
+    url: `${process.env.VUE_APP_BASE_URL || 'https://hall-blog.herokuapp.com/'}`
+  },
+  api: {
+    url: `${process.env.VUE_APP_API_URL ||
+      'https://jsonplaceholder.typicode.com'}`
   }
 }
