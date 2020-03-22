c7d16b393889e9503882b17eda371375d7ec9100
allan_oliveira <allanadriiano@gmail.com>
2020-03-21 23:34:50 -0300

    adjustments general site

---
+++ components/BoxShare.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="box-share-component">
+    <p>Compartilhe em suas redes:</p>
+    <span>
+      <a href="#">Facebook</a>
+      <a href="#">Twitter</a>
+      <a :href="`https://wa.me/?text=`" target="_blank">Whatsapp</a>
+    </span>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'BoxShare',
+  props: {
+    url: {
+      type: Object
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
--- services/BlogService.js
+++ services/BlogService.js
@@ -3,7 +3,7 @@ import axios from '~/plugins/axios'
 export default {
   async getAllArticles(data) {
     const response = await axios.get('/posts', data)
-    return response.data
+    return response.data.slice(0, 5)
   },
   async getSingleArticle(id, data) {
     const response = await axios.get('/posts/' + id, data)
